095d3f776f74b62283db54593e7b29e31f23df0c
Dndyytr <dandy.taufiqurrochman@gmail.com>
2026-06-29 18:42:30 +0700

    hosting vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
 
+// Fix Symfony request base URL detection when executing from /api/index.php
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
 // Forward Vercel requests to normal Laravel public/index.php
 require __DIR__.'/../public/index.php';
